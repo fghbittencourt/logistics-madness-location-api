77d3882c91a2e9c0a3785905d2aa7b4ad2ffacb4
aaronmorlocks <aaronmorlocks@gmail.com>
2021-03-11 00:54:06 -0300

    Adicionar dependencia do mongo e inserir no banco de testes no atlas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-require('dotenv/config');
+import dotenv from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
 
+dotenv.config()
 
 const port = process.env.PORT;
 const app = express();
 
+mongoose.connect(process.env.DB, { useUnifiedTopology: true, useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("🤗🤗🤗 MongoDB database connection established successfully");
+});
+
+const Schema = mongoose.Schema;
+
+let locationSchema = new Schema(
+  {
+    clientID: {
+      type: String
+    },
+    date:{
+      type: Date
+    }
+  },
+);
+
+const Location = mongoose.model("location-history", locationSchema);
+
+app.post('/location', (req, res) => {
+  Location.insertMany({clientID: "Fernando", date: new Date()})
+  res.send("OK!")
+})
 
 app.get("/", (req, res) => {
   res.json({msg: "Olá"});
